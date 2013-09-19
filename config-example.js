0bb8370e6fdd9b706f76588f4ca7ad3f56a45368
Stefan Thomas <justmoon@members.fsf.org>
2013-09-19 09:51:37 -0700

    Cleanup.

--- config-example.js
+++ config-example.js
@@ -2,5 +2,21 @@
 // exports.host = '127.0.0.1';
 exports.host = null;
 
+// Port to listen on
+exports.port = 8080;
+
+// SSL settings
+exports.ssl = false;
+
 // Whether this blob vault is running behind a reverse proxy
 exports.is_proxy = false;
+
+// Database settings
+exports.mysql = {
+  host: 'localhost',
+  port: '3306',
+  database: 'blob_vault',
+  user: 'blobby',
+  password: '57umtSMG4Fyv5ary'
+};
+
--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 var config = require('./config');
 
+var http = require('http');
 var https = require('https');
 var fs = require('fs');
 var mysql = require('mysql');
 var express = require('express');
 
-var c = mysql.createConnection({
-  host: 'localhost',
-  port: '3306',
-  database: 'blob_vault',
-  user: 'blobby',
-  password: '57umtSMG4Fyv5ary'
-});
+var c = mysql.createConnection(config.mysql);
 c.connect();
 
 var app = express();
@@ -19,38 +14,34 @@ app.use(express.bodyParser());
 
 app.get('/:key', function (req, res) {
 	console.log("app.get");
-	
-	try{
-  res.set({
-    'Content-Type': 'text/plain',
-    'Access-Control-Allow-Origin': '*'
-  });
-  
-  c.query(
-    "SELECT v FROM blobs WHERE k = ? LIMIT 1",
-    [req.params.key],
-    function (err, qres) {
-      res.send(qres.length ? qres[0].v : "");
-    }
-  );
-	}catch(e)
-	{
+
+	try {
+    res.set({
+      'Content-Type': 'text/plain',
+      'Access-Control-Allow-Origin': '*'
+    });
+
+    c.query(
+      "SELECT v FROM blobs WHERE k = ? LIMIT 1",
+      [req.params.key],
+      function (err, qres) {
+        res.send(qres.length ? qres[0].v : "");
+      }
+    );
+	} catch(e) {
 		console.log("Exception: "+e);
-		c.connect(); 
+		c.connect();
 	}
-  
 });
 
 var sjcl = require('sjcl');
 function verifies(pubKey, sig, data) {
- 
     var curve = sjcl.ecc.curves.c192,
         pubBits = sjcl.codec.base64.toBits(pubKey),
         pubKey = new sjcl.ecc.ecdsa.publicKey(curve, pubBits),
         sigBits = sjcl.codec.base64.toBits(sig);
 
     return pubKey.verify(sjcl.hash.sha256.hash(data), sigBits);
- 
 }
 
 function insert_blob(req) {
@@ -66,40 +57,37 @@ function insert_blob(req) {
 }
 
 app.post('/:key', function (req, res) {
-	try{
-  res.set({
-    'Content-Type': 'text/plain',
-    'Access-Control-Allow-Origin': '*'
-  });
+	try {
+    res.set({
+      'Content-Type': 'text/plain',
+      'Access-Control-Allow-Origin': '*'
+    });
 
-  c.query(
-    "SELECT pub_key FROM blobs WHERE k = ? LIMIT 1",
-    [req.params.key],
-    function (err, qres) {
-      if (qres.length && qres[0].pub_key) {
-        verifies(qres[0].pub_key, req.body.sig, req.body.blob) && insert_blob(req);
-      } else {
-        insert_blob(req);
+    c.query(
+      "SELECT pub_key FROM blobs WHERE k = ? LIMIT 1",
+      [req.params.key],
+      function (err, qres) {
+        if (qres.length && qres[0].pub_key) {
+          verifies(qres[0].pub_key, req.body.sig, req.body.blob) && insert_blob(req);
+        } else {
+          insert_blob(req);
+        }
+        res.send();
       }
-      res.send();
-    }
-  )
-	}catch(e)
-	{
+    );
+	} catch(e) {
 		console.log("Exception: "+e);
-		c.connect(); 
+		c.connect();
 	}
 });
 
-app.listen(80, config.host);
-
 try {
-  var https = https.createServer({
+  var server = config.ssl ? https.createServer({
     key: fs.readFileSync(__dirname + '/blobvault.key'),
     ca: fs.readFileSync(__dirname + '/intermediate.crt'),
     cert: fs.readFileSync(__dirname + '/blobvault.crt')
-  }, app);
-  https.listen(443, config.host);
+  }, app) : http.createServer(app);
+  server.listen(config.port || (config.ssl ? 443 : 8080), config.host);
 } catch (e) {
   console.log("Could not launch SSL server: " + (e.stack ? e.stack : e.toString()));
 }
