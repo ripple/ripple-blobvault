0af2a7955e42c552b62ec7fb7240c948dbef0cc9
David Wee <rook2pawn@gmail.com>
2014-04-15 17:10:32 -0700

    cors

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use(function(req,res,next) {
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // JSON handlers
 app.post('/v1/user', api.blob.create);
 app.delete('/v1/user', hmac.middleware, api.blob.delete);
--- test/test-blob-patch.js
+++ test/test-blob-patch.js
@@ -4,6 +4,7 @@ var express = require('express');
 var store = require('../lib/store')(config.dbtype);
 var hmac = require('../lib/hmac');
 var api = require('../api');
+var testutils = require('./utils');
 var request = require('request');
 api.setStore(store);
 hmac.setStore(store);
@@ -20,20 +21,18 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.post('/v1/blob/patch', hmac.middleware, api.blob.patch);
+
+var sig = testutils.createSignature({method:'DELETE',url:'/v1/blob/patch',secret:testutils.person.auth_secret,date:'april'});
 var server = http.createServer(app);
 server.listen(5050);
 
 q.series([
 // needs blob_id and patch
     function(lib) {
+        var url = 'http://localhost:5050/v1/blob/patch?signature=' + sig + '&signature_date=april&signature_blob_id='+ testutils.person.blob_id;
+        console.log("Url:" + url);
         request.post({
-            url:'http://localhost:5050/v1/blob/patch?signature=7d496acc44105db1f1fd6e0a56e66d905d66bd512ade6a6a4c6b609d202f6728ccbbc1034babcb5368662599b291948ec2c7345ee3db42cf81cd3929f7634097&signature_date=april&signature_blob_id=ffff0a0affff0a0affff0a0affff0a0affff0a0affff0a0affff0a0affff0a0a',
-            json:{
-                blob_id:'ffff0a0affff0a0affff0a0affff0a0affff0a0affff0a0affff0a0affff0a0a',    
-                patch : "cat",
-                zebra: 'is an animal',
-                hotdog : 'is food'
-            }
+            url:url
         },function(err, resp, body) {
             console.log("The response");
                 log(err);
