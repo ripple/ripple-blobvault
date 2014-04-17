b663ccf27f0c11401c2153fe4931003c3081946b
David Wee <rook2pawn@gmail.com>
2014-04-17 00:42:55 -0700

    run node migration.js to migrate

---
+++ migration.js
@@ -0,0 +1,25 @@
+var config = require('./config');
+var Knex = require('knex');
+
+if (config.dbtype == 'memory') {
+    console.log("migration for in memory not supported")
+    process.exit()
+} else if ((config.dbtype !== 'postgres') || (config.dbtype !== 'mysql')) {
+    console.log("config.dbtype: " + config.dbtype + ' is neither postgres nor mysql. No migration')
+    process.exit()
+}
+
+var knex = Knex.initialize({
+    client: config.dbtype,
+    connection : config.database.postgres
+});
+// add fields
+knex.schema.table('blob', function (table) {
+    /*
+    table.dropColumn('name'); // remove column 
+    table.string('first_name'); // add a column called first_name, type string, etc
+    table.string('last_name');
+    */
+}).then(function () {
+    console.log('blob is migrated!');
+});
