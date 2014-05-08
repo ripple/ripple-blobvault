89294854141d9a82cc6cb5a7d3d904d521228fec
David Wee <rook2pawn@gmail.com>
2014-05-08 03:29:57 -0700

    campaign work

--- testcampaign.js
+++ testcampaign.js
@@ -1,11 +1,32 @@
 var EC = require('./lib/emailcampaign');
 var config = require('./config');
+var QL = require('queuelib');
 var Knex = require('knex');
 var db = Knex.initialize({
     client: 'postgres',
     connection : config.database.postgres
 });
-var ec = new EC(db,config)
-ec.start(function() {
-    console.log("Connected");
-});
+
+var q = new QL;
+q.series([
+
+function(lib) {
+    db('campaigns')
+        .truncate()
+        .then(function(resp) {
+            console.log(arguments);
+            lib.done();
+        })
+        .catch(function(e) {
+            console.log(e);
+        });
+},
+
+function(lib) {
+    var ec = new EC(db,config)
+    ec.start(function() {
+        console.log("Connected");
+        lib.done();
+    });
+}
+])
