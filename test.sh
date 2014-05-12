dc47ab1657594010e51bcb3ff6e51dc3b43d82ac
David Wee <rook2pawn@gmail.com>
2014-05-12 07:28:56 -0700

    unified status code for travis ci

--- test.sh
+++ test.sh
@@ -1,12 +1,10 @@
 # find ./test -name "test*.js" -exec node_modules/.bin/mocha --ui tdd -R spec '{}' \;
-node_modules/.bin/mocha --ui tdd -R spec test/test-user-create.js
-node_modules/.bin/mocha --ui tdd -R spec test/test-user-get.js
-node_modules/.bin/mocha --ui tdd -R spec test/test-user-caseinsensitivity-and-uniqueness.js
-node_modules/.bin/mocha --ui tdd -R spec test/test-blob-patch.js
-node_modules/.bin/mocha --ui tdd -R spec test/test-signature.js
-node_modules/.bin/mocha --ui tdd -R spec test/test-usercap.js
-node_modules/.bin/mocha --ui tdd -R spec test/test-quota.js
-node_modules/.bin/mocha --ui tdd -R spec test/test-normalize-reserved.js
-
-
+node_modules/.bin/mocha --ui tdd -R spec test/test-user-create.js &&
+node_modules/.bin/mocha --ui tdd -R spec test/test-user-get.js &&
+node_modules/.bin/mocha --ui tdd -R spec test/test-user-caseinsensitivity-and-uniqueness.js && 
+node_modules/.bin/mocha --ui tdd -R spec test/test-blob-patch.js &&
+node_modules/.bin/mocha --ui tdd -R spec test/test-signature.js &&
+node_modules/.bin/mocha --ui tdd -R spec test/test-usercap.js &&
+node_modules/.bin/mocha --ui tdd -R spec test/test-quota.js &&
+node_modules/.bin/mocha --ui tdd -R spec test/test-normalize-reserved.js &&
 node_modules/.bin/mocha --ui tdd -R spec test/test-email-change-resend.js
