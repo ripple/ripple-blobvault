dc883293607d354972dd4a4c1384f302e5ea8c1c
David Wee <rook2pawn@gmail.com>
2014-08-20 23:45:57 -0700

    adapted payload for jwt

--- api/requestAttestation.js
+++ api/requestAttestation.js
@@ -5,6 +5,8 @@ var config = require('../config');
 var client = require('blockscore')(config.blockscore.key);
 var jwtSigner = require('jwt-sign');
 var key = require('fs').readFileSync('./test.pem', 'utf8');
+var conformParams = require('../lib/conformParams')
+
 
 var requestAttestation = function(req,res,next) {
   
@@ -21,149 +23,119 @@ var requestAttestation = function(req,res,next) {
     7. save trust score into identity table
     8. return attestation JWT and trust score
   */
- 
-  //get from query string params for now
-  var result = conformParams(req.body);
-  var code;
-  
-  //console.log(result);
-  
-  if (result.error) {
-    response.json({result:'error', message:result.error}).status(400).pipe(res); 
-    return; 
-  } 
-  
-  client.verifications.create(result.params, function (err, resp) {
-    var result;
-    //console.log(err, resp);
-    
-    if (err) {
-      code   = 400;
-      result = {
-        result  : 'error',
-        message : err.message,
-        error   : err.param + ": " + err.code
-      };
-      
-    } else if (resp.status === 'invalid') {
-      
-      //possibly handle invalid attestation differently
-      code   = 200;
-      result = {
-        result  : 'success',
-        status  : resp.status,
-        details : resp.details
-      };
-                  
-    //formulate attestation
-    } else { 
 
-      var payload = {
-        iss : "https://id.ripple.com",
-        sub : req.params.identity_id,
-        exp : ~~(new Date().getTime() / 1000) + (30 * 60),
-        iat : ~~(new Date().getTime() / 1000 - 60),
-        given_name  : req.body.given_name,
-        family_name : req.body.family_name,
-        birthdate   : req.body.birthdate,
-        address : {
-          line1       : req.body.address.line1,
-          locality    : req.body.address.locality,
-          region      : req.body.address.region,
-          postal_code : req.body.address.postal_code,
-          country     : req.body.address.country
-        }
-      };
-      
-      if (req.body.address.line2) payload.address.line2 = req.body.address.line2;  
-      if (req.body.ssn_last_4)    payload.ssn_last_4    = req.body.ssn_last_4;
-      if (req.body.passport)      payload.passport      = req.body.passport;
-      if (req.body.middle_name)   payload.middle_name   = req.body.middle_name;
-      if (req.body.phone)         params.phone          = req.body.phone;
-      if (req.body.ip)            params.ip_address     = req.body.ip;
-    
-      
-      payload.address_match   = resp.details.address;
-      payload.address_risk    = resp.details.address_risk;
-      payload.birthdate_match = resp.details.date_of_birth;
-      payload.ofac_match      = resp.details.ofac;
-      payload.pep_match       = resp.details.pep;
-      
-  
-     
-      if (req.body.ssn_last_4) {
-        payload.ssn_last_4_match = resp.details.identification;
-      } else if (req.body.ssn) {
-        payload.ssn_match = resp.details.identification;
-      } else if (req.body.passport) {
-        payload.passport_match = resp.details.identification;
-      }
-      
+    var identity_id = req.params.identity_id
+    reporter.log("requestAttestation:identity_id:", identity_id)
+    var q = new Queue
+    q.series([
+    function(lib) {
+        exports.store.read_where({table:'identity_attributes',key:'identity_id', value:identity_id},
+        function(resp) {
+            reporter.log("getProfile:attributes lookup response:", resp)
+            lib.set({attributes:resp})
+            lib.done()
+        });
+    },
+    function(lib) {
+        exports.store.read_where({table:'identity_addresses',key:'identity_id', value:identity_id},
+        function(resp) {
+            reporter.log("getProfile:addresses lookup response:", resp)
+            lib.set({addresses:resp})
+            lib.done()
+        });
+    },
+    function(lib) {
+        //get from query string params for now
+        var data = {addresses:lib.get('addresses'),attributes:lib.get('attributes')} 
+        var result = conformParams(data);
 
-      //TODO: catch error with signing
-      var signed = jwtSigner.sign(payload, key); 
-      
-      console.log(payload);
-      code   = 200;
-      result = {
-        result  : 'success',
-        claims_token : signed
-      };
+        var code;
+
+        //console.log(result);
+
+        if (result.error) {
+        response.json({result:'error', message:result.error}).status(400).pipe(res); 
+        lib.terminate()
+        return; 
+        } 
+        var params = result.params;
+
+        client.verifications.create(params,
+        function (err, resp) {
+            var result;
+            //console.log(err, resp);
+            if (err) {
+                code   = 400;
+                result = {
+                result  : 'error',
+                message : err.message,
+                error   : err.param + ": " + err.code
+                }
+            } else if (resp.status === 'invalid') {
+                //possibly handle invalid attestation differently
+                code   = 200;
+                result = {
+                    result  : 'success',
+                    status  : resp.status,
+                    details : resp.details
+                };
+            //formulate attestation
+            } else { 
+                var payload = {
+                    iss : "https://id.ripple.com",
+                    sub : req.params.identity_id,
+                    exp : ~~(new Date().getTime() / 1000) + (30 * 60),
+                    iat : ~~(new Date().getTime() / 1000 - 60),
+                    given_name  : params.first,
+                    family_name : params.last,
+                    birthdate   : params.date_of_birth,
+                    address : {
+                        line1       : params.address.street1,
+                        locality    : params.address.city,
+                        region      : params.address.state,
+                        postal_code : params.address.postal_code,
+                        country     : params.address.country_code
+                    }
+                };
+                if (params.address.street2) 
+                payload.address.line2 = params.address.street2;  
+                if (params.identification.ssn)
+                payload.ssn_last_4 = params.identification.ssn;
+                if (params.identification.passport)
+                payload.passport = params.identification.passport
+                if (params.phone_number)
+                payload.phone = params.phone_number
+                
+                if (params.ip_address) 
+                payload.ip_address = params.ip_address
+              
+                payload.address_match   = resp.details.address;
+                payload.address_risk    = resp.details.address_risk;
+                payload.birthdate_match = resp.details.date_of_birth;
+                payload.ofac_match      = resp.details.ofac;
+                payload.pep_match       = resp.details.pep;
+             
+                if (params.ssn_last_4) {
+                    payload.ssn_last_4_match = resp.details.identification;
+                } else if (params.passport) {
+                    payload.passport_match = resp.details.identification;
+                }
+
+                //TODO: catch error with signing
+                var signed = jwtSigner.sign(payload, key); 
+
+                console.log(payload);
+                code   = 200;
+                result = {
+                result  : 'success',
+                claims_token : signed
+                };
+            }
+            response.json(result).status(code).pipe(res);  
+            lib.done()
+        });
     }
-    
-    response.json(result).status(code).pipe(res);  
-  });
-  
-  
-
-  function conformParams (data) {
-    var params = {
-      identification : { },
-      name           : { },
-      address        : { },
-    };
-    
-    if (!req.body.given_name) {
-      return {error:"given_name is required"};
-    } else if (!req.body.family_name) {
-      return {error:"family_name is required"};
-    } else if (!req.body.birthdate) {
-      return {error:"birthdate is required"};
-    } else if (!req.body.ssn_last_4 && !req.body.passport) {
-      return {error:"either ssn_last_4 or passport is required"};
-    } else if (!req.body.address) {
-      return {error:"address is required"};
-    }  else if (!req.body.address.line1) {
-      return {error:"address.line1 is required"};
-    }  else if (!req.body.address.locality) {
-      return {error:"address.locality (city/locality) is required"};
-    }  else if (!req.body.address.region) {
-      return {error:"address.region (state/province/region) is required"};
-    }  else if (!req.body.address.postal_code) {
-      return {error:"address.postal_code is required"};
-    }  else if (!req.body.address.country) {
-      return {error:"address.country (ISO code) is required"};
-    }  
-    
-    params.name.first           = req.body.given_name;
-    params.name.last            = req.body.family_name;
-    params.date_of_birth        = req.body.birthdate;
-    params.address.street1      = req.body.address.line1; 
-    params.address.city         = req.body.address.locality; 
-    params.address.state        = req.body.address.region; 
-    params.address.postal_code  = req.body.address.postal_code; 
-    params.address.country_code = req.body.address.country;  
-         
-    if (req.body.address.line2) params.address.street2         = req.body.address.line2;  
-    if (req.body.ssn_last_4)    params.identification.ssn      = req.body.ssn_last_4;
-    if (req.body.passport)      params.identification.passport = req.body.passport;
-    if (req.body.middle_name)   params.name.middle             = req.body.middle_name;
-    if (req.body.phone)         params.phone_number            = req.body.phone;
-    if (req.body.ip)            params.ip_address              = req.body.ip;
-      
-    return {params:params};
-  }
-
+    ])
 }
 
 module.exports = exports = requestAttestation;
