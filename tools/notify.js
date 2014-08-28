838edcdc4bee61e2a3907f71e9d65f8237995fa4
David Wee <rook2pawn@gmail.com>
2014-08-28 13:28:37 -0700

    2fa ready notification

---
+++ tools/notify.js
@@ -0,0 +1,74 @@
+var config = require('../config');
+var fs = require('fs');
+var email     = require('emailjs');
+var server     = email.server.connect({
+   user:    config.email.user,
+   password:config.email.password,
+   host:    config.email.host,
+   ssl: true
+});
+var store = require('../lib/store')(config.dbtype);
+var Queue = require('queuelib')
+var q = new Queue;
+
+
+    var contents = fs.readFileSync(__dirname+ '/2faready.html');
+    var contents_txt = fs.readFileSync(__dirname+ '/2faready.txt', 'utf8');
+    var generateMessage = function(email,username) {
+        var message    = {
+           text:    contents_txt.replace(/_USERNAME_/gi, username),
+           from:    "you <foo@bar.com>",
+           to:        "someone <foo@bar.com>",
+           subject:    "Ripple - Two Factor Authentication is here!",
+           'Reply-to' : 'support@ripple.com',
+           attachment:
+           [
+            {
+                data:undefined,
+                alternative:true
+            }
+           ]
+        };
+        if ((config.is_staging !== undefined) && (config.is_staging)) {
+            message.subject = '[Staging] ' + message.subject;
+        }
+        message.attachment[0].data = contents.toString().replace(/_USERNAME_/gi, username);
+        message.to = username + " <" + email + ">";
+        message.from = config.email.from;
+        return message;
+    }
+
+var success = []
+var failure = []
+
+q.series([
+    function(lib) {
+        store.db('blob')
+        .select('email','username')
+        .then(function(resp) {
+            lib.set({list:resp})
+            lib.done()
+        })
+    },
+    function(lib) {
+        var mailqueue = new Queue;
+        var list = lib.get('list')
+        mailqueue.forEach(list, function(item,idx, lib2) {
+            var message = generateMessage(item.email, item.username)
+            server.send(message, function(err, message) {
+                console.log(err, message)
+                if (!err) {
+                    success.push(item)
+                } else 
+                    failure.push(item)
+                lib2.done()
+            });
+        },
+        function() {
+            console.log("All done sending out 2fa ready emails")
+            console.log("failures:" , failure)           
+            console.log("Success:", success)
+            lib.done()
+        }, 1000)
+    }
+])
