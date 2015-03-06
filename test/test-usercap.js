3845c04840391320eb5f893b7ac7a1fa599f62cf
Stefan Thomas <justmoon@members.fsf.org>
2015-03-06 13:53:00 -0800

    [TASK] Formatting.

--- test/test-usercap.js
+++ test/test-usercap.js
@@ -11,29 +11,31 @@ suite('count limiter', function() {
     })
 
     test('should have a thousand count',function() {
-        for (var i = 0; i < 1004; i++) {
-            count.add()
-            var chk = count.check();
-            if (i >= 1000)
-                assert.equal(chk, false,'after 1000, no more')
-            else
-                assert.equal(chk, true,'the first 1000 work')
+      for (var i = 0; i < 1004; i++) {
+        count.add();
+        var chk = count.check();
+        if (i >= 1000) {
+          assert.equal(chk, false, 'after 1000, no more');
+        } else {
+          assert.equal(chk, true, 'the first 1000 work');
         }
-        for (var i = 0; i < 1004; i++) {
-            count.add()
-            var chk = count.check();
-            assert.equal(chk,false,'the next thousand or so should fail')
+      }
+      for (var i = 0; i < 1004; i++) {
+        count.add();
+        var chk = count.check();
+        assert.equal(chk, false, 'the next thousand or so should fail');
+      }
+      var oneday = 1000*60*60*24;
+      this.clock.tick(oneday+1);
+      for (var i = 0; i < 1004; i++) {
+        count.add();
+        var chk = count.check();
+        if (i >= 1000) {
+          assert.equal(chk, false, 'after one day, after 1000 fail');
+        } else {
+          assert.equal(chk, true, 'after one day, the first 1000 work');
         }
-        var oneday = 1000*60*60*24;
-        this.clock.tick(oneday+1);
-        for (var i = 0; i < 1004; i++) {
-            count.add()
-            var chk = count.check();
-            if (i >= 1000)
-                assert.equal(chk, false,'after one day, after 1000 fail')
-            else
-                assert.equal(chk, true,'after one day, the first 1000 work')
-        }
+      }
     });
 
     suiteTeardown(function () {
